70e872417d89192e1d684eb012ba241a849f6536
Bikram Sahoo <bikram.sahoo@waiin.com>
2023-08-19 23:01:46 +0530

    added api section

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //api/users
+public class UsersController: ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public UsersController(DataContext dataContext){
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
+        var users = await _dataContext.Users.ToListAsync();
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id){
+        var user = await _dataContext.Users.FindAsync(id);
+        return user;
+    }
+}
